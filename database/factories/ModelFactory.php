35ca7a01055f1ee0e38694a20a2b2b077a222fe6
ddaLogin@gmail.com <h4c2o2R2d2>
2017-08-26 17:00:09 +0600

    Update base migrations
                          Install Tymon\JWT for api auth
                          Update base migrations

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,7 +16,8 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'name' => $faker->firstName,
+        'surname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,8 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('surname')->nullable()->default(null);
+            $table->string('email', 150)->unique();
+            $table->string('password', 150);
             $table->rememberToken();
             $table->timestamps();
         });
