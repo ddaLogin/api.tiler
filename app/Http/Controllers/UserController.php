7f56afcd2bd8b8113fc8b76b7de96d17741e2453
ddaLogin@gmail.com <h4c2o2R2d2>
2017-09-18 20:42:59 +0600

    Fix access denied error on user update API

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -131,8 +131,12 @@ class UserController extends ApiController
      */
     public function update(UpdateUserRequest $request, User $user)
     {
-        if (JWTAuth::parseToken()->authenticate()->id != $user->id || !Auth::validate(['email' => $user->email, 'password' => $request->get('current_password')])){
-            throw new AccessDeniedHttpException();
+        if (JWTAuth::parseToken()->authenticate()->id != $user->id){
+            return response()->json(trans('accessDenied'), 403);
+        }
+
+        if (!Auth::validate(['email' => $user->email, 'password' => $request->get('current_password')])){
+            return response()->json(trans('wrongPassword'), 403);
         }
 
         $user = $this->userService->update($request->except('current_password'), $user->id);
---
+++ resources/lang/en/app.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'accessDenied' => "You don't have permission",
+    'wrongPassword' => 'The password does not match',
+];
